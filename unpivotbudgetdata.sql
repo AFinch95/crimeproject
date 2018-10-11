8037d1dd3ad9793d781f42442b66984caae9ba15
Alex Finch <alexrfinch@gmail.com>
2018-10-11 12:11:47 +0100

    Added two missing financial years

--- unpivotbudgetdata.sql
+++ unpivotbudgetdata.sql
@@ -1,7 +1,7 @@
 SELECT
-	 F1 as Borough
-	,[Financial Year]
-	,Budget
+	 F1 as borough
+	,[financialyear]
+	,budget
 FROM 
 (SELECT
 	*
@@ -9,5 +9,6 @@ FROM
 	[raw].[youthservicesbudgetsraw]
 ) r
 unpivot(
-Budget for [Financial Year] in ([2011/12], [2012/13], [2013/14], [2014/15], [2016/17], [2018/19])
+Budget for [financialyear] in ([2011/12], [2012/13], [2013/14], [2014/15], [2015/16], [2016/17], [2017/18], [2018/19])
 ) budget
+
