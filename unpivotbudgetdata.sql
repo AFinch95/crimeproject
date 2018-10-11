059cf3b522765eb8833ca8121d6197e3db24032f
Alex Finch <alexrfinch@gmail.com>
2018-10-11 12:14:56 +0100

    unpivottedstaffnosdata

--- unpivotbudgetdata.sql
+++ unpivotbudgetdata.sql
@@ -12,3 +12,16 @@ unpivot(
 Budget for [financialyear] in ([2011/12], [2012/13], [2013/14], [2014/15], [2015/16], [2016/17], [2017/18], [2018/19])
 ) budget
 
+SELECT
+	 F1 as borough
+	,[financialyear]
+	,staffnos
+FROM 
+(SELECT
+	*
+FROM
+	[raw].[noofstaffraw]
+) r
+unpivot(
+staffnos for [financialyear] in ([2011/12], [2012/13], [2013/14], [2014/15], [2015/16], [2016/17], [2017/18], [2018/19])
+) staff
