4e6dc49f03a92550aed580941bba958f8e53f32d
Alex Finch <alexrfinch@gmail.com>
2018-10-12 09:18:16 +0100

    changed name to include trash schema

---
+++ createschemas.sql
@@ -0,0 +1,33 @@
+use crimeproject;
+go
+
+-- create schema for raw data
+create schema [raw]
+;
+
+--transfer all raw data to raw schema
+alter schema [raw] transfer [dbo].[crimedataraw]
+;
+
+alter schema [raw] transfer [dbo].[noofstaffraw]
+;
+
+alter schema [raw] transfer [dbo].[noofyouthcentresraw]
+;
+
+alter schema [raw] transfer [dbo].[yclocationsraw]
+;
+
+alter schema [raw] transfer [dbo].[youthservicesbudgetsraw]
+;
+
+-- create schema for trash
+create schema [trash]
+;
+
+--transfer all trash
+alter schema [trash] transfer [dbo].[LondonLSOAcodes]
+;
+
+alter schema [trash] transfer [raw].[crimedataraw]
+;
