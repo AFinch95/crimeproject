88afbe05b5caa062f02380d9a7659558c8cf0d34
Alex Finch <alexrfinch@gmail.com>
2018-10-17 14:29:03 +0100

    Added unpivot for staff numbers

--- unpivotbudgetdata.sql
+++ unpivotbudgetdata.sql
@@ -1,3 +1,9 @@
+-- Unpivoting all YC data
+-- Unpivoting budget data
+if OBJECT_ID('[raw].[YCbudgetunpivot]') is not null
+drop table [raw].[YCbudgetunpivot]
+
+;with CTEbudget as(
 SELECT
 	 F1 as borough
 	,[financialyear]
@@ -11,7 +17,17 @@ FROM
 unpivot(
 Budget for [financialyear] in ([2011/12], [2012/13], [2013/14], [2014/15], [2015/16], [2016/17], [2017/18], [2018/19])
 ) budget
+)
+SELECT * 
+INTO [raw].[YCbudgetunpivot]
+FROM CTEbudget
 
+-- Unpivot staff table
+
+if OBJECT_ID('[raw].[YCstaffnosunpivot]') is not null
+drop table [raw].[YCstaffnosunpivot]
+
+;with CTEstaff as (
 SELECT
 	 F1 as borough
 	,[financialyear]
@@ -25,3 +41,9 @@ FROM
 unpivot(
 staffnos for [financialyear] in ([2011/12], [2012/13], [2013/14], [2014/15], [2015/16], [2016/17], [2017/18], [2018/19])
 ) staff
+)
+SELECT *
+INTO [raw].[YCstaffnosunpivot]
+FROM CTEstaff
+
+SELECT [F8] FROM [raw].[yclocationsraw]
